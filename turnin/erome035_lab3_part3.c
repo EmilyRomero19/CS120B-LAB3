8c00c400865f14d18c52045bb48b21c2d653bcda
EmilyRomero19 <76976095+EmilyRomero19@users.noreply.github.com>
2021-04-12 20:00:03 -0700

    Update erome035_lab3_part3.c

--- turnin/erome035_lab3_part3.c
+++ turnin/erome035_lab3_part3.c
@@ -7,10 +7,6 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
@@ -18,3 +14,5 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0x00; PORTB = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
+    
+    
